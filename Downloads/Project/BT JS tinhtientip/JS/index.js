97810125b14d3c4ef928ce11cd78469138974632
toan <thanhtoan12x3clc@gmail.com>
2024-05-11 22:43:53 +0700

    commit so 1

---
+++ Downloads/Project/BT JS tinhtientip/JS/index.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("btn").onclick = function(){
+var soNguoi = Number(document.getElementById("so_Nguoi").value)
+}
+
+
+
+
+
+tongTienTip = function(tiLe, soNguoi){
+    var tienTip = (tongTien + tongTien * tiLe)/soNguoi;
+
+}
