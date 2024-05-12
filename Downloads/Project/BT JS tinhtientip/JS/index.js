2d619414f99b8b1ca17fd64dabec6ca1b592648a
toan <thanhtoan12x3clc@gmail.com>
2024-05-12 22:33:36 +0700

    commit so 2

--- Downloads/Project/BT JS tinhtientip/JS/index.js
+++ Downloads/Project/BT JS tinhtientip/JS/index.js
@@ -1,14 +1,19 @@
 
 
-document.getElementById("btn").onclick = function(){
-var soNguoi = Number(document.getElementById("so_Nguoi").value)
+document.getElementById("btn").onclick = function () {
+    var soNguoi = Number(document.getElementById("so_Nguoi").value);
+    var tiLe = Number(document.getElementById("select").value);
+    var tongTien = Number(document.getElementById("sotien").value);
+    document.getElementById("output").innerHTML = tongTienTip(tiLe, soNguoi, tongTien) + "$"
+    
 }
 
 
 
 
 
-tongTienTip = function(tiLe, soNguoi){
-    var tienTip = (tongTien + tongTien * tiLe)/soNguoi;
+tongTienTip = function (tiLe, soNguoi, tongTien) {
+    var tienTip = (tongTien * (tiLe/100))/soNguoi;
+    return tienTip;
 
 }
